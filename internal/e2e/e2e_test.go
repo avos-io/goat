1b4e4ed6490ca78670cd0b8a2a089defd039c9fe
Matt <matt@avos.io>
2022-11-21 18:21:05 +0000

    e2e tests for interceptors

--- internal/e2e/e2e_test.go
+++ internal/e2e/e2e_test.go
@@ -7,9 +7,12 @@ import (
 	"io"
 	"testing"
 
+	grpcMiddleware "github.com/grpc-ecosystem/go-grpc-middleware"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
 
 	"github.com/avos-io/goat/gen/testproto"
 	"github.com/avos-io/goat/gen/testproto/mocks"
@@ -27,14 +30,14 @@ func TestUnary(t *testing.T) {
 		send := &testproto.Msg{Value: 42}
 		exp := &testproto.Msg{Value: 9001}
 
-		ms := mocks.NewTestServiceServer(t)
-		ms.EXPECT().Unary(mock.Anything, mock.MatchedBy(
+		service := mocks.NewTestServiceServer(t)
+		service.EXPECT().Unary(mock.Anything, mock.MatchedBy(
 			func(msg *testproto.Msg) bool {
 				return msg.GetValue() == send.GetValue()
 			},
 		)).Return(exp, nil)
 
-		client, ctx, teardown := setup(ms)
+		client, ctx, teardown := setup(service)
 		defer teardown()
 
 		reply, err := client.Unary(ctx, send)
@@ -46,10 +49,10 @@ func TestUnary(t *testing.T) {
 	t.Run("Error", func(t *testing.T) {
 		is := require.New(t)
 
-		ms := mocks.NewTestServiceServer(t)
-		ms.EXPECT().Unary(mock.Anything, mock.Anything).Return(nil, errTest)
+		service := mocks.NewTestServiceServer(t)
+		service.EXPECT().Unary(mock.Anything, mock.Anything).Return(nil, errTest)
 
-		client, ctx, teardown := setup(ms)
+		client, ctx, teardown := setup(service)
 		defer teardown()
 
 		reply, err := client.Unary(ctx, &testproto.Msg{Value: 4})
@@ -132,8 +135,7 @@ func TestClientStream(t *testing.T) {
 					is.NoError(err)
 					sum += msg.GetValue()
 				}
-			},
-			).
+			}).
 			Return(nil)
 
 		client, ctx, teardown := setup(service)
@@ -234,7 +236,367 @@ func TestBidiStream(t *testing.T) {
 	})
 }
 
+func TestUnaryInterceptor(t *testing.T) {
+	is := require.New(t)
+
+	clientInterceptor := func(
+		ctx context.Context,
+		method string,
+		req, reply interface{},
+		cc *grpc.ClientConn,
+		invoker grpc.UnaryInvoker,
+		opts ...grpc.CallOption,
+	) error {
+		ctx = metadata.AppendToOutgoingContext(ctx, "foo", "1")
+		return invoker(ctx, method, req, reply, cc, opts...)
+	}
+
+	serverInterceptor := func(
+		ctx context.Context,
+		req interface{},
+		info *grpc.UnaryServerInfo,
+		handler grpc.UnaryHandler,
+	) (interface{}, error) {
+		md, ok := metadata.FromIncomingContext(ctx)
+		is.True(ok)
+
+		foo, ok := md["foo"]
+		is.True(ok)
+		is.Len(foo, 1)
+		is.Equal("1", foo[0])
+
+		md.Append("bar", "2")
+		newCtx := metadata.NewIncomingContext(ctx, md)
+		return handler(newCtx, req)
+	}
+
+	service := mocks.NewTestServiceServer(t)
+	service.EXPECT().Unary(mock.Anything, mock.Anything).
+		Run(func(ctx context.Context, msg *testproto.Msg) {
+			md, ok := metadata.FromIncomingContext(ctx)
+			is.True(ok)
+
+			foo, ok := md["foo"]
+			is.True(ok)
+			is.Len(foo, 1)
+			is.Equal("1", foo[0])
+
+			bar, ok := md["bar"]
+			is.True(ok)
+			is.Len(bar, 1)
+			is.Equal("2", bar[0])
+		}).
+		Return(&testproto.Msg{Value: 1}, nil)
+
+	client, ctx, teardown := setupOpts(
+		service,
+		[]client.DialOption{
+			client.WithUnaryInterceptor(clientInterceptor),
+		},
+		[]server.ServerOption{
+			server.UnaryInterceptor(serverInterceptor),
+		},
+	)
+	defer teardown()
+
+	reply, err := client.Unary(ctx, &testproto.Msg{Value: 42})
+	is.NoError(err)
+	is.NotNil(reply)
+}
+
+func TestChainUnaryInterceptor(t *testing.T) {
+	is := require.New(t)
+
+	makeClientInterceptor := func(k, v string) grpc.UnaryClientInterceptor {
+		return func(
+			ctx context.Context,
+			method string,
+			req, reply interface{},
+			cc *grpc.ClientConn,
+			invoker grpc.UnaryInvoker,
+			opts ...grpc.CallOption,
+		) error {
+			ctx = metadata.AppendToOutgoingContext(ctx, k, v)
+			return invoker(ctx, method, req, reply, cc, opts...)
+		}
+	}
+
+	clientMd := map[string]string{
+		"one":   "1",
+		"two":   "2",
+		"three": "3",
+	}
+
+	clientInterceptors := []grpc.UnaryClientInterceptor{}
+	for k, v := range clientMd {
+		clientInterceptors = append(clientInterceptors, makeClientInterceptor(k, v))
+	}
+
+	makeServerInterceptor := func(k, v string) grpc.UnaryServerInterceptor {
+		return func(
+			ctx context.Context,
+			req interface{},
+			info *grpc.UnaryServerInfo,
+			handler grpc.UnaryHandler,
+		) (interface{}, error) {
+			md, ok := metadata.FromIncomingContext(ctx)
+			is.True(ok)
+
+			for ek, ev := range clientMd {
+				match, ok := md[ek]
+				is.True(ok)
+				is.Equal(ev, match[0])
+			}
+
+			md.Append(k, v)
+			newCtx := metadata.NewIncomingContext(ctx, md)
+			return handler(newCtx, req)
+		}
+	}
+
+	serverMd := map[string]string{
+		"ein":  "1",
+		"zwei": "2",
+		"drei": "3",
+	}
+
+	serverInterceptors := []grpc.UnaryServerInterceptor{}
+	for k, v := range serverMd {
+		serverInterceptors = append(serverInterceptors, makeServerInterceptor(k, v))
+	}
+
+	service := mocks.NewTestServiceServer(t)
+	service.EXPECT().Unary(mock.Anything, mock.Anything).
+		Run(func(ctx context.Context, msg *testproto.Msg) {
+			md, ok := metadata.FromIncomingContext(ctx)
+			is.True(ok)
+
+			for ek, ev := range clientMd {
+				match, ok := md[ek]
+				is.True(ok)
+				is.Equal(ev, match[0])
+			}
+
+			for ek, ev := range serverMd {
+				match, ok := md[ek]
+				is.True(ok)
+				is.Equal(ev, match[0])
+			}
+		}).
+		Return(&testproto.Msg{Value: 1}, nil)
+
+	client, ctx, teardown := setupOpts(
+		service,
+		[]client.DialOption{
+			client.WithChainUnaryInterceptor(clientInterceptors...),
+		},
+		[]server.ServerOption{
+			server.UnaryInterceptor(
+				grpcMiddleware.ChainUnaryServer(serverInterceptors...),
+			),
+		},
+	)
+	defer teardown()
+
+	reply, err := client.Unary(ctx, &testproto.Msg{Value: 42})
+	is.NoError(err)
+	is.NotNil(reply)
+}
+
+func TestStreamInterceptor(t *testing.T) {
+	is := require.New(t)
+
+	clientInterceptor := func(
+		ctx context.Context,
+		desc *grpc.StreamDesc,
+		cc *grpc.ClientConn,
+		method string,
+		streamer grpc.Streamer,
+		opts ...grpc.CallOption,
+	) (grpc.ClientStream, error) {
+		ctx = metadata.AppendToOutgoingContext(ctx, "foo", "1")
+		return streamer(ctx, desc, cc, method, opts...)
+	}
+
+	serverInterceptor := func(
+		srv interface{},
+		stream grpc.ServerStream,
+		info *grpc.StreamServerInfo,
+		handler grpc.StreamHandler,
+	) error {
+		ctx := stream.Context()
+		md, ok := metadata.FromIncomingContext(ctx)
+		is.True(ok)
+
+		foo, ok := md["foo"]
+		is.True(ok)
+		is.Len(foo, 1)
+		is.Equal("1", foo[0])
+
+		md.Append("bar", "2")
+		newCtx := metadata.NewIncomingContext(ctx, md)
+
+		return handler(srv, &grpcMiddleware.WrappedServerStream{
+			ServerStream:   stream,
+			WrappedContext: newCtx,
+		})
+	}
+
+	service := mocks.NewTestServiceServer(t)
+	service.EXPECT().ServerStream(mock.Anything, mock.Anything).
+		Run(func(_ *testproto.Msg, stream testproto.TestService_ServerStreamServer) {
+			md, ok := metadata.FromIncomingContext(stream.Context())
+			is.True(ok)
+
+			foo, ok := md["foo"]
+			is.True(ok)
+			is.Len(foo, 1)
+			is.Equal("1", foo[0])
+
+			bar, ok := md["bar"]
+			is.True(ok)
+			is.Len(bar, 1)
+			is.Equal("2", bar[0])
+
+			stream.Send(&testproto.Msg{Value: 42})
+		}).
+		Return(nil)
+
+	client, ctx, teardown := setupOpts(
+		service,
+		[]client.DialOption{
+			client.WithStreamInterceptor(clientInterceptor),
+		},
+		[]server.ServerOption{
+			server.StreamInterceptor(serverInterceptor),
+		},
+	)
+	defer teardown()
+
+	stream, err := client.ServerStream(ctx, &testproto.Msg{Value: 42})
+	is.NoError(err)
+	is.NotNil(stream)
+
+	stream.Recv()
+}
+
+func TestChainStreamInterceptor(t *testing.T) {
+	is := require.New(t)
+
+	makeClientInterceptor := func(k, v string) grpc.StreamClientInterceptor {
+		return func(
+			ctx context.Context,
+			desc *grpc.StreamDesc,
+			cc *grpc.ClientConn,
+			method string,
+			streamer grpc.Streamer,
+			opts ...grpc.CallOption,
+		) (grpc.ClientStream, error) {
+			ctx = metadata.AppendToOutgoingContext(ctx, k, v)
+			return streamer(ctx, desc, cc, method, opts...)
+		}
+	}
+
+	clientMd := map[string]string{
+		"one":   "1",
+		"two":   "2",
+		"three": "3",
+	}
+
+	clientInterceptors := []grpc.StreamClientInterceptor{}
+	for k, v := range clientMd {
+		clientInterceptors = append(clientInterceptors, makeClientInterceptor(k, v))
+	}
+
+	makeServerInterceptor := func(k, v string) grpc.StreamServerInterceptor {
+		return func(
+			srv interface{},
+			stream grpc.ServerStream,
+			info *grpc.StreamServerInfo,
+			handler grpc.StreamHandler,
+		) error {
+			ctx := stream.Context()
+			md, ok := metadata.FromIncomingContext(ctx)
+			is.True(ok)
+
+			for ek, ev := range clientMd {
+				match, ok := md[ek]
+				is.True(ok)
+				is.Equal(ev, match[0])
+			}
+
+			md.Append(k, v)
+			newCtx := metadata.NewIncomingContext(ctx, md)
+
+			return handler(srv, &grpcMiddleware.WrappedServerStream{
+				ServerStream:   stream,
+				WrappedContext: newCtx,
+			})
+		}
+	}
+
+	serverMd := map[string]string{
+		"ein":  "1",
+		"zwei": "2",
+		"drei": "3",
+	}
+
+	serverInterceptors := []grpc.StreamServerInterceptor{}
+	for k, v := range serverMd {
+		serverInterceptors = append(serverInterceptors, makeServerInterceptor(k, v))
+	}
+
+	service := mocks.NewTestServiceServer(t)
+	service.EXPECT().ServerStream(mock.Anything, mock.Anything).
+		Run(func(_ *testproto.Msg, stream testproto.TestService_ServerStreamServer) {
+			md, ok := metadata.FromIncomingContext(stream.Context())
+			is.True(ok)
+
+			for ek, ev := range clientMd {
+				match, ok := md[ek]
+				is.True(ok)
+				is.Equal(ev, match[0])
+			}
+
+			for ek, ev := range serverMd {
+				match, ok := md[ek]
+				is.True(ok)
+				is.Equal(ev, match[0])
+			}
+
+			stream.Send(&testproto.Msg{Value: 42})
+		}).
+		Return(nil)
+
+	client, ctx, teardown := setupOpts(
+		service,
+		[]client.DialOption{
+			client.WithChainStreamInterceptor(clientInterceptors...),
+		},
+		[]server.ServerOption{
+			server.StreamInterceptor(
+				grpcMiddleware.ChainStreamServer(serverInterceptors...),
+			),
+		},
+	)
+	defer teardown()
+
+	stream, err := client.ServerStream(ctx, &testproto.Msg{Value: 42})
+	is.NoError(err)
+	is.NotNil(stream)
+
+	stream.Recv()
+}
+
 func setup(s testproto.TestServiceServer) (testproto.TestServiceClient, context.Context, func()) {
+	return setupOpts(s, nil, nil)
+}
+
+func setupOpts(
+	s testproto.TestServiceServer,
+	clientOpts []client.DialOption,
+	serverOpts []server.ServerOption,
+) (testproto.TestServiceClient, context.Context, func()) {
 	ctx, cancel := context.WithCancel(context.Background())
 
 	serverConn := testutil.NewTestConn()
@@ -266,7 +628,7 @@ func setup(s testproto.TestServiceServer) (testproto.TestServiceClient, context.
 		}
 	}()
 
-	server := server.NewServer()
+	server := server.NewServer(serverOpts...)
 	testproto.RegisterTestServiceServer(server, s)
 
 	go func() {
@@ -274,7 +636,7 @@ func setup(s testproto.TestServiceServer) (testproto.TestServiceClient, context.
 	}()
 
 	client := testproto.NewTestServiceClient(
-		client.NewClientConn(clientConn),
+		client.NewClientConn(clientConn, clientOpts...),
 	)
 
 	teardown := func() {
