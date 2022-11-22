578306b57d4255b00b0dcb8cf248f798a0cbc675
Matt <matt@avos.io>
2022-11-22 11:54:08 +0000

    Remove websocket code

--- goat.go
+++ goat.go
@@ -2,20 +2,20 @@ package goat
 
 import (
 	"context"
-	"fmt"
-
-	"google.golang.org/grpc/encoding"
-	"google.golang.org/grpc/encoding/proto"
-	"nhooyr.io/websocket"
 
 	wrapped "github.com/avos-io/goat/gen"
 )
 
+// RpcReadWriter is the generic interface used by Goat's client and servers.
+// It utilises the wrapped.Rpc protobuf format for generically wrapping gRPC
+// calls.
 type RpcReadWriter interface {
 	Read(context.Context) (*wrapped.Rpc, error)
 	Write(context.Context, *wrapped.Rpc) error
 }
 
+// NewFnReadWriter is a convenience wrapper to turn read and write functions
+// into an RpcReadWriter.
 func NewFnReadWriter(
 	r func(context.Context) (*wrapped.Rpc, error),
 	w func(context.Context, *wrapped.Rpc) error,
@@ -34,45 +34,4 @@ func (frw *fnReadWriter) Read(ctx context.Context) (*wrapped.Rpc, error) {
 
 func (frw *fnReadWriter) Write(ctx context.Context, rpc *wrapped.Rpc) error {
 	return frw.w(ctx, rpc)
-}
-
-// NOTE: this wouldn't actually live in the same package, but it's here for now
-type WebsocketRpcReadWriter struct {
-	conn  *websocket.Conn
-	codec encoding.Codec
-}
-
-func NewWebsocketRpcReadWriter(conn *websocket.Conn) RpcReadWriter {
-	return &WebsocketRpcReadWriter{
-		conn:  conn,
-		codec: encoding.GetCodec(proto.Name),
-	}
-}
-
-func (wrw *WebsocketRpcReadWriter) Read(ctx context.Context) (*wrapped.Rpc, error) {
-	msgType, data, err := wrw.conn.Read(ctx)
-
-	if err != nil {
-		return nil, err
-	}
-
-	if msgType != websocket.MessageBinary {
-		return nil, fmt.Errorf("WebsocketRpcReadWriter: invalid recv type %d", msgType)
-	}
-
-	var rpc wrapped.Rpc
-	err = wrw.codec.Unmarshal(data, &rpc)
-	if err != nil {
-		return nil, err
-	}
-
-	return &rpc, nil
-}
-
-func (wrw *WebsocketRpcReadWriter) Write(ctx context.Context, rpc *wrapped.Rpc) error {
-	bytes, err := wrw.codec.Marshal(rpc)
-	if err != nil {
-		return err
-	}
-	return wrw.conn.Write(ctx, websocket.MessageBinary, bytes)
 }
