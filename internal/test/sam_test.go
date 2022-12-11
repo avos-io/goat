f33e2a9f6250e7561c287efedbcd57fcb63ced7f
Sam Jansen <sam@avos.io>
2022-12-11 05:15:58 +0000

    Move code about

--- internal/test/sam_test.go
+++ internal/test/sam_test.go
@@ -6,80 +6,16 @@ package sam
 
 import (
 	"context"
-	"encoding/binary"
-	"io"
 	"net"
-	"sync"
 	"testing"
 
 	"github.com/avos-io/goat"
 	"github.com/avos-io/goat/client"
-	wrapped "github.com/avos-io/goat/gen"
 	"github.com/avos-io/goat/gen/testproto"
 	"github.com/avos-io/goat/server"
 	"github.com/stretchr/testify/require"
 	"google.golang.org/grpc"
-	"google.golang.org/protobuf/proto"
 )
-
-type goatOverPipe struct {
-	conn       net.Conn
-	readMutex  sync.Mutex
-	writeMutex sync.Mutex
-}
-
-func newGoatOverPipe(c net.Conn) *goatOverPipe {
-	return &goatOverPipe{conn: c}
-}
-
-func (g *goatOverPipe) Read(ctx context.Context) (*wrapped.Rpc, error) {
-	g.readMutex.Lock()
-	defer g.readMutex.Unlock()
-
-	// XXX: no timeout support here
-	var msgSize uint32
-	err := binary.Read(g.conn, binary.BigEndian, &msgSize)
-	if err != nil {
-		panic(err)
-	}
-
-	data := make([]byte, msgSize)
-	_, err = io.ReadFull(g.conn, data)
-	if err != nil {
-		panic(err)
-	}
-
-	var rpc wrapped.Rpc
-	err = proto.Unmarshal(data, &rpc)
-	if err != nil {
-		panic(err)
-	}
-
-	return &rpc, nil
-}
-
-func (g *goatOverPipe) Write(ctx context.Context, pkt *wrapped.Rpc) error {
-	g.writeMutex.Lock()
-	defer g.writeMutex.Unlock()
-
-	data, err := proto.Marshal(pkt)
-	if err != nil {
-		panic(err)
-	}
-
-	var msgSize uint32 = uint32(len(data))
-	err = binary.Write(g.conn, binary.BigEndian, &msgSize)
-	if err != nil {
-		panic(err)
-	}
-
-	_, err = g.conn.Write(data)
-	if err != nil {
-		panic(err)
-	}
-
-	return nil
-}
 
 type simulatedServer struct {
 	srv *server.Server
@@ -145,7 +81,7 @@ func (*echoServer) Unary(_ context.Context, m *testproto.Msg) (*testproto.Msg, e
 	return m, nil
 }
 
-func TestSam(t *testing.T) {
+func TestGoatOverPipes(t *testing.T) {
 	// Make a pipe for communication between Proxy and Server
 	ps1, ps2 := net.Pipe()
 	// And between Client and Proxy
