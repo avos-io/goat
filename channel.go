46195f820202cae788ff954f14cf8d09f9cd60c3
Matt <matt@avos.io>
2023-01-12 09:34:56 +0000

    Add a HTTP Goat Read Writer

--- channel.go
+++ channel.go
@@ -7,45 +7,8 @@ import (
 	"github.com/avos-io/goat/internal"
 )
 
-type ReadReturn struct {
-	Rpc *Rpc
-	Err error
-}
-
-// NewGoatOverChannel is a convenience wrapper to turn a read channel and a
-// write channel into an RpcReadWriter
-func NewGoatOverChannel(inQ chan ReadReturn, outQ chan *Rpc) RpcReadWriter {
-	read := func(ctx context.Context) (*Rpc, error) {
-		select {
-		case <-ctx.Done():
-			return nil, ctx.Err()
-		case ret, ok := <-inQ:
-			if !ok {
-				return nil, fmt.Errorf("read channel closed")
-			}
-			if ret.Err != nil {
-				return nil, ret.Err
-			}
-			return ret.Rpc, nil
-		}
-	}
-
-	write := func(ctx context.Context, rpc *Rpc) error {
-		select {
-		case <-ctx.Done():
-			return ctx.Err()
-		case outQ <- rpc:
-			return nil
-		}
-	}
-
-	return internal.NewFnReadWriter(read, write)
-}
-
-// TODO(mjb):
-// Can't decide whether I need errors explicitly in the channel item, or if
-// closing the channel on errors will be sufficient...
-func NewGoatOverChannelNoErrors(inQ chan *Rpc, outQ chan *Rpc) RpcReadWriter {
+// NewGoatOverChannel turns an input and an output channel into an RpcReadWriter.
+func NewGoatOverChannel(inQ chan *Rpc, outQ chan *Rpc) RpcReadWriter {
 	read := func(ctx context.Context) (*Rpc, error) {
 		select {
 		case <-ctx.Done():
