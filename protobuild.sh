d0d1a667ccd9adabb1f0c0cab354144614d7c6ca
Dominik Strak <dominik@avos.io>
2023-09-18 15:41:26 +0100

    buf build for goat

--- protobuild.sh
+++ protobuild.sh
@@ -20,4 +20,4 @@ mkdir -p gen
 protoc --go_out=gen --go_opt=paths=source_relative --go-grpc_out=gen --go-grpc_opt=paths=source_relative ./goatorepo/rpc.proto $grpc_opts
 
 mkdir -p gen/testproto
-protoc --go_out=gen/testproto --go_opt=paths=source_relative --go-grpc_out=gen/testproto --go-grpc_opt=paths=source_relative ./test.proto $grpc_opts
+protoc --go_out=gen/testproto --go_opt=paths=source_relative --go-grpc_out=gen/testproto --go-grpc_opt=paths=source_relative ./testproto/test.proto $grpc_opts
