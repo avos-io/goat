665d3e17d884511e558467158e3d466e6c8417f1
Sam Jansen <sam@avos.io>
2024-02-29 18:18:08 +0000

    Client stream test

--- client_test.go
+++ client_test.go
@@ -78,4 +78,77 @@ func TestClientStatsHandler(t *testing.T) {
 
 		<-done
 	})
+
+	t.Run("streaming", func(t *testing.T) {
+		is := require.New(t)
+
+		statsMock := grpcStatsMocks.NewMockHandler(t)
+
+		expectStatsHandleConn[*stats.ConnBegin](statsMock).Times(1)
+		expectStatsHandleConn[*stats.ConnEnd](statsMock).Times(1)
+
+		expectStatsHandleRPC[*stats.Begin](statsMock).Times(1)
+		expectStatsHandleRPC[*stats.InHeader](statsMock).Times(1)
+		expectStatsHandleRPC[*stats.InPayload](statsMock).Times(1)
+		expectStatsHandleRPC[*stats.End](statsMock).Times(1)
+		expectStatsHandleRPC[*stats.OutHeader](statsMock).Times(1)
+		expectStatsHandleRPC[*stats.OutPayload](statsMock).Times(1)
+		//expectStatsHandleRPC[*stats.OutTrailer](statsMock).Times(1)
+
+		tagRPCCall := statsMock.EXPECT().TagRPC(mock.Anything, mock.Anything)
+		tagRPCCall.Run(func(ctx context.Context, tag *stats.RPCTagInfo) {
+			ctx2 := metadata.AppendToOutgoingContext(ctx, "from-tag-rpc", "test 1")
+			tagRPCCall.Return(ctx2)
+		}).Times(1)
+		statsMock.EXPECT().TagConn(mock.Anything, mock.Anything).Times(1).Return(context.Background())
+
+		srv := NewServer("s0")
+		defer srv.Stop()
+
+		service := mocks.NewMockTestServiceServer(t)
+		service.EXPECT().BidiStream(mock.Anything).Run(func(ss testproto.TestService_BidiStreamServer) {
+			msg, err := ss.Recv()
+			is.NoError(err)
+
+			is.Equal(int32(42), msg.GetValue())
+
+			err = ss.Send(msg)
+			is.NoError(err)
+		}).Return(nil).Times(1)
+
+		testproto.RegisterTestServiceServer(srv, service)
+
+		ps1, ps2 := net.Pipe()
+		done := make(chan struct{})
+
+		go func() {
+			srv.Serve(context.Background(), testutil.NewGoatOverPipe(ps1))
+			done <- struct{}{}
+		}()
+
+		clientConn := NewClientConn(testutil.NewGoatOverPipe(ps2), "c0", "s0", WithStatsHandler(statsMock))
+		cl := testproto.NewTestServiceClient(clientConn)
+
+		client, err := cl.BidiStream(context.Background())
+		is.NoError(err)
+
+		err = client.Send(&testproto.Msg{Value: 42})
+		is.NoError(err)
+
+		msg, err := client.Recv()
+		is.NoError(err)
+
+		is.Equal(int32(42), msg.GetValue())
+
+		// This should die - the server will finish the streaming RPC
+		_, err = client.Recv()
+		is.Error(err)
+
+		clientConn.Close()
+
+		ps1.Close()
+		ps2.Close()
+
+		<-done
+	})
 }
